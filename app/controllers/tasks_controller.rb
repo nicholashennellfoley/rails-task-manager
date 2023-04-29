efebb15ebdd34822d3ea401c05ef17ee609a14c4
Nick Hennell-Foley <nicholas@nicholashennellfoley.com>
2023-04-29 15:44:42 +0100

    Added links :)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,7 +26,7 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_path, status: :see_other
+    redirect_to tasks_path status: :see_other
   end
 
   private
